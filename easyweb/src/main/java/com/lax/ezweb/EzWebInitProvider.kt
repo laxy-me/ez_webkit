103a638eefd388185efedc248c5b8a0983653c7b
yangguangda <514939946@qq.com>
2020-08-21 18:02:46 +0800

    二元项目lib

--- easyweb/src/main/java/com/lax/ezweb/EzWebInitProvider.kt
+++ easyweb/src/main/java/com/lax/ezweb/EzWebInitProvider.kt
@@ -11,18 +11,11 @@ import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.annotation.Keep
-import com.adjust.sdk.Adjust
-import com.adjust.sdk.AdjustConfig
 import com.facebook.appevents.AppEventsLogger
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.iid.FirebaseInstanceId
-import com.igexin.sdk.PushManager
-import com.lax.ezweb.service.MyPushService
-import com.lax.ezweb.service.PushIntentService
 import com.lax.ezweb.tools.AppInfo
 import com.lax.ezweb.tools.ToastUtil
-import com.umeng.analytics.MobclickAgent
-import com.umeng.commonsdk.UMConfigure
 import io.branch.referral.Branch
 
 
@@ -47,11 +40,8 @@ class EzWebInitProvider : ContentProvider() {
                 Preference.init(application)
                 ToastUtil.init(application)
                 GetGpsIdTask().execute(application)
-                initAdjust(application)
                 initBranch(application)
-                initUm(application)
                 initFacebook(application)
-                initPush(application)
                 initFCM()
             } catch (e: Exception) {
                 e.stackTrace
@@ -78,66 +68,8 @@ class EzWebInitProvider : ContentProvider() {
         AppEventsLogger.activateApp(application)
     }
 
-    private fun initUm(application: Application) {
-        val um = AppInfo.getMetaData(application, "UMENG_APP_KEY")
-        if (um.isNotBlank()) {
-            UMConfigure.setLogEnabled(BuildConfig.DEBUG)
-            UMConfigure.init(application, UMConfigure.DEVICE_TYPE_PHONE, "")
-            // 选用AUTO页面采集模式
-            MobclickAgent.setPageCollectionMode(MobclickAgent.PageMode.AUTO);
-        }
-    }
-
-    private fun initPush(context: Context) {
-        val pushAppId = AppInfo.getMetaData(context, "PUSH_APPID")
-        if (pushAppId.isNotBlank()) {
-            //for google play store
-            PushManager.getInstance()
-                .registerPushIntentService(context, PushIntentService::class.java)
-            PushManager.getInstance().initialize(context, MyPushService::class.java)
-            PushManager.getInstance().setPrivacyPolicyStrategy(context, true)
-
-            //2.14.0.0 for other markets
-//            PushManager.getInstance().initialize(context)
-        }
-    }
-
     private fun initBranch(application: Application) {
         Branch.getAutoInstance(application)
-    }
-
-    private fun initAdjust(application: Application) {
-        val appToken = AppInfo.getMetaData(application, "ADJUST_APPTOKEN")
-        if (appToken.isNotBlank()) {
-            val environment = AdjustConfig.ENVIRONMENT_PRODUCTION
-            val config = AdjustConfig(application, appToken, environment, false)
-            val adjustTracker = AppInfo.getMetaData(application, "ADJUST_TRACK_TOKEN")
-            if (adjustTracker.isNotBlank()) {
-                config.setDefaultTracker(adjustTracker)
-            }
-            Adjust.onCreate(config)
-            application.registerActivityLifecycleCallbacks(AdjustLifecycleCallbacks())
-        }
-    }
-
-    private class AdjustLifecycleCallbacks : Application.ActivityLifecycleCallbacks {
-        override fun onActivityResumed(activity: Activity) {
-            Adjust.onResume()
-        }
-
-        override fun onActivityPaused(activity: Activity) {
-            Adjust.onPause()
-        }
-
-        override fun onActivityStopped(activity: Activity) {}
-
-        override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle?) {}
-
-        override fun onActivityDestroyed(activity: Activity) {}
-
-        override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {}
-
-        override fun onActivityStarted(activity: Activity) {}
     }
 
     override fun insert(uri: Uri, values: ContentValues?): Uri? {
